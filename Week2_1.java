cca1428d927657160519fd6225e78867cfdba88d
Sandeepedupalli18 <117476103+Sandeepedupalli18@users.noreply.github.com>
2023-01-20 10:01:34 +0530

    Add files via upload

---
+++ Week2_1.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+public class Week2_1
+{
+public static void main(String args[])
+{
+Area a=new Area();
+a.read();
+a.area();
+a.display();
+}
+}
+class Area
+{
+static int l,b,area;
+static void read()
+{
+System.out.println("enter dimensions of a rectangle");
+Scanner s=new Scanner(System.in);
+l=s.nextInt();
+b=s.nextInt();
+}
+static void area()
+{
+area=l*b;
+}
+static void display()
+{
+System.out.println("Area="+area);
+}
+}
